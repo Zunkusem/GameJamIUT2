b29dea8279910e393b54305fdcfe7bf15006badc
Zunkusem <jeremi04@hotmail.fr>
2019-02-04 18:20:46 +0100

    Modif mouveent et debut de mise ne place hitbox
    Prise en conte de la hit box a faire

--- protoJérémi/TestGZK.py
+++ protoJérémi/TestGZK.py
@@ -1,8 +1,22 @@
 import pygame
 
-GRAVITY = 0  # Pretty low gravity.
+GRAVITY = 0.2  # Pretty low gravity.
 XS = .1
 listeB = []
+
+class Block(pygame.sprite.Sprite):
+
+    def __init__(self, pos, screen):
+        super().__init__()
+        lenght = 100
+        height = 50
+        self.screen = screen
+        self.image = self.image = pygame.Surface((lenght, height), pygame.SRCALPHA)
+        pygame.draw.rect(self.image, (255, 255, 255), pygame.rect.Rect((0, 0, 100, 50)))
+        self.rect = self.image.get_rect(center=pos)
+        self.hittop = [pos[0],pos[1],pos[0]+lenght,pos[1]]
+
+
 
 class Circle(pygame.sprite.Sprite):
 
@@ -10,7 +24,7 @@ class Circle(pygame.sprite.Sprite):
         super().__init__()
         self.screen = screen
         self.image = pygame.Surface((80, 80), pygame.SRCALPHA)
-        pygame.draw.circle(self.image, (30, 90, 150), (40, 40), 40)
+        pygame.draw.rect(self.image, (30, 90, 150), pygame.rect.Rect((0, 0, 25, 25)))
         self.rect = self.image.get_rect(center=pos)
         self.pos_y = pos[1]
         self.pos_x = pos[0]
@@ -19,7 +33,7 @@ class Circle(pygame.sprite.Sprite):
         self.xs = 0
         listeB.append(self)
 
-    def update(self):
+    def update(self,block):
         self.speed_y += GRAVITY
         self.pos_y += self.speed_y
         self.rect.y = self.pos_y
@@ -30,12 +44,21 @@ class Circle(pygame.sprite.Sprite):
         elif self.speed_x < 0:
             self.speed_x += XS
 
-        if self.speed_y > 0:
-            self.speed_y -= XS
-        elif self.speed_y < 0:
-            self.speed_y += XS
+        # if self.speed_y > 0:
+        #     self.speed_y -= XS
+        # elif self.speed_y < 0:
+        #     self.speed_y += XS
+
+        if self.pos_y > 600:
+            self.speed_y = 0
+            self.speed_y -= 0.2
+
+        for value in variable:
+            pass
 
         if self.pos_y > self.screen.get_height():
+            self.kill()  # Remove off-screen circles.
+        if self.pos_y > self.screen.get_width():
             self.kill()  # Remove off-screen circles.
 
     def moveymin(self):
@@ -50,43 +73,43 @@ class Circle(pygame.sprite.Sprite):
     def movexmin(self):
         self.speed_x = -5
 
-
-
 def run_game():
     pygame.init()
     screen = pygame.display.set_mode((1270, 670))
     clock = pygame.time.Clock()
     running = True
     circles = pygame.sprite.Group(Circle((600, 0), screen))
+    blocks = pygame.sprite.Group(Block((0,600), screen))
 
     while running:
         pressed = pygame.key.get_pressed()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.type == pygame.MOUSEBUTTONDOWN:
                 circles.add(Circle(event.pos, screen))
-            elif pressed[pygame.K_d]:
+            if pressed[pygame.K_d]:
                 for i in range(0,len(listeB)):
                     listeB[i].movex()
                 pass
-            elif pressed[pygame.K_a]:
+            if pressed[pygame.K_a]:
                 for i in range(0,len(listeB)):
                     listeB[i].movexmin()
                 pass
-            elif pressed[pygame.K_w]:
+            if pressed[pygame.K_w]:
                 for i in range(0,len(listeB)):
                     listeB[i].movey()
                 pass
-            elif pressed[pygame.K_s]:
+            if pressed[pygame.K_s]:
                 for i in range(0,len(listeB)):
                     listeB[i].moveymin()
                 pass
 
-        circles.update()
+        circles.update(blocks)
 
         screen.fill((10, 10, 30))
         circles.draw(screen)
+        blocks.draw(screen)
 
         pygame.display.flip()
         clock.tick(60)
