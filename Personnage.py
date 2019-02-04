dc174f6c8080e4dc8c553343b29dd277c0d201cb
EmilioBe098 <39830161+EmilioBe098@users.noreply.github.com>
2019-02-04 13:48:00 +0100

    Premiere version classe personnage

---
+++ Personnage.py
@@ -0,0 +1,10 @@
+class Personnage: # Définition de notre classe Personne
+
+    def __init__(self): # Notre méthode constructeur
+        self.positionX = 10
+        self.positionY = 10
+        self.largeur = 10
+        self.hauteur = 10
+        self.vulnerabilite = False
+        self.vecteurVitesse = array(1,1)
+        
