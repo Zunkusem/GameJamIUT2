8dd51376d7f834b0dd7dc7cc3ca3297addd6a3b4
Zunkusem <jeremi04@hotmail.fr>
2019-02-05 10:17:15 +0100

    Mise en place de la fragmentation par obj

---
+++ protoJérémi/G1/block.py
@@ -0,0 +1,17 @@
+import pygame
+GRAVITY = 0.2  # Pretty low gravity.
+XS = .1
+class Block(pygame.sprite.Sprite):
+
+    def __init__(self, pos, screen):
+        super().__init__()
+        lenght = 100
+        height = 50
+        self.screen = screen
+        self.image = self.image = pygame.Surface((lenght, 50), pygame.SRCALPHA)
+        pygame.draw.rect(self.image, (255, 255, 255), pygame.rect.Rect((0, 0, lenght, height)))
+        self.rect = self.image.get_rect(center=pos)
+        self.hittop = [pos[0]-(lenght/2)-25,pos[1]-(lenght/2)+5,(pos[0]+lenght)-(lenght/2),pos[1]]
+        self.hitleft = [pos[0]-(lenght/2)-25,pos[1]-(lenght/2)+10,pos[0]-(lenght/2)-25,(pos[1]+height)-(height/2)]
+        self.hitright = [(pos[0]+lenght)-(lenght/2),pos[1],(pos[0]+lenght)-(lenght/2),(pos[1]+height)-(height/2)]
+        self.hitbottom = [pos[0]-(lenght/2)-25,(pos[1]+height)-(height/2),(pos[0]+lenght)-(lenght/2),(pos[1]+height)-(height/2)]
---
+++ protoJérémi/G1/main.py
@@ -0,0 +1,57 @@
+import pygame
+GRAVITY = 0.2  # Pretty low gravity.
+XS = .1
+
+from player import *
+from block import *
+
+def run_game():
+    pygame.init()
+    screen = pygame.display.set_mode((1270, 670))
+    pygame.display.set_caption("Scrolling Camera")
+    clock = pygame.time.Clock()
+    running = True
+    circles = pygame.sprite.Group(Player((600, 0), screen))
+    blocks = pygame.sprite.Group(Block((600,600), screen))
+    blocks.add(Block((690,600), screen))
+    blocks.add(Block((690,200), screen))
+    blocks.add(Block((780,620), screen))
+    blocks.add(Block((510,620), screen))
+
+    while running:
+        pressed = pygame.key.get_pressed()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                circles.add(Player(event.pos, screen))
+        pass
+        if pressed[pygame.K_d]:
+            for i in circles:
+                i.movex()
+            pass
+        if pressed[pygame.K_a]:
+            for i in circles:
+                i.movexmin()
+            pass
+        if pressed[pygame.K_w]:
+            for i in circles:
+                i.movey()
+            pass
+        if pressed[pygame.K_s]:
+            for i in circles:
+                i.moveymin()
+            pass
+
+        circles.update(blocks)
+
+        screen.fill((10, 10, 30))
+        circles.draw(screen)
+        blocks.draw(screen)
+
+        pygame.display.flip()
+        clock.tick(60)
+
+
+run_game()
+pygame.quit()
