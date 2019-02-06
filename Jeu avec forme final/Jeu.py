c7f154dfee22f8a2f390d3804c01756ca41ff45b
EmilioBe098 <39830161+EmilioBe098@users.noreply.github.com>
2019-02-06 18:17:05 +0100

    ajout score

--- Jeu avec forme final/Jeu.py
+++ Jeu avec forme final/Jeu.py
@@ -1,8 +1,10 @@
 import pygame
+from pygame import *
 from math import *
 from Player import *
 from Map import *
 from Param import *
+from Score import *
 
 
 def calculDeLaVitesseProjectile(x1,y1,x2,y2):# (x,y) position du tireur (x1,y1) position de la cible
@@ -38,9 +40,18 @@ def main():
 
     # Create the player
     player = Player()
-
+    score = Score()
+    
     bullets = pygame.sprite.Group()
 
+    #pour l'affichage du score
+    
+    font.init()
+    font_a = pygame.font.SysFont('arial', 40)
+    font_b = pygame.font.SysFont('arial', 70)
+    
+    
+    
     # Create all the levels
     level_list = []
     level_list.append(Level_01(player))
@@ -156,6 +167,14 @@ def main():
         # Limit to 60 frames per second
         clock.tick(60)
 
+        #affichage score
+        score.incrementeScoreDeUn()
+        font_score = font_a.render(score.getScore(), 1, (YELLOW))
+        font_multiplicateur = font_a.render(score.getMultiplicateur(), 1, (ORANGE))
+        font_temps = font_a.render("00:00", 1, (255,255,255))
+        screen.blit(font_score, (900,10))
+        screen.blit(font_multiplicateur, (950,10))
+        screen.blit(font_temps, (500,10))
         # Go ahead and update the screen with what we've drawn.
         pygame.display.flip()
 
