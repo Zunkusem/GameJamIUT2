6558e9711bae13b539cdcf366d5b8dee19d0fd57
Faurepeq <39625490+Faurepeq@users.noreply.github.com>
2019-02-07 22:57:00 +0100

    encore des lignes effacer

--- Jeu avec forme final/Jeu.py
+++ Jeu avec forme final/Jeu.py
@@ -9,7 +9,6 @@ from Score import *
 from Fin import *
 import Fin
 
-
 def main():
     """ Main Program """
     pygame.init()
@@ -78,7 +77,7 @@ def main():
             if event.type == pygame.QUIT:
                 done = True
                 pygame.quit()
-                
+
             elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 print("x:",event.pos[0]," y:",event.pos[1])
                 xSouris=event.pos[0]
@@ -100,8 +99,7 @@ def main():
                     player.jump()
                 if event.key == pygame.K_SPACE:
                     player.invG()
-                if event.key == pygame.K_ESCAPE:
-                    launcher()
+
 
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_a and player.change_x < 0:
@@ -113,7 +111,7 @@ def main():
         active_sprite_list.update(screen)
 
         # Update items in the level
-        current_level.update(player,screen)
+        current_level.update(player)
         # current_level.shift_worldy(player.change_y,player)
 
         # If the player gets near the right side, shift the world left (-x)
@@ -179,7 +177,6 @@ def main():
     # Be IDLE friendly. If you forget this line, the program will 'hang'
     # on exit.
     score_final = player.level.score.getScore() #a decommenter lors du rassemblage avec forpec
-    #score_final = 100
     fin(score_final)
     #return player.level.score.getScore() #a decommenter lors du rassemblage avec forpec
     pygame.quit()
