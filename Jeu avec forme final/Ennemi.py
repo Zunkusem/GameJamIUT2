6825b083d071865b5590e372aa05503da5eb446f
EmilioBe098 <39830161+EmilioBe098@users.noreply.github.com>
2019-02-07 09:10:49 +0100

    deplacement score dans map

--- Jeu avec forme final/Ennemi.py
+++ Jeu avec forme final/Ennemi.py
@@ -75,3 +75,37 @@ class Tourelle(pygame.sprite.Sprite):
             vitesseX,vitesseY=calculDeLaVitesseProjectile(self.rect.right,self.rect.centery,player.rect.x,player.rect.y)
             bullets.add(Bullet(self.rect.right, self.rect.centery, vitesseX, vitesseY))
             self.countdonw = 60
+
+
+
+            
+
+class EnnemiDetecteur(pygame.sprite.Sprite):
+    """ Platform the user can jump on """
+
+    def __init__(self):
+        """ Platform constructor. Assumes constructed with user passing in
+            an array of 5 numbers like what's defined at the top of this code.
+            """
+        super().__init__()
+        width = 100
+        height = 100
+        self.hp = 5
+        self.image = pygame.Surface([width, height])
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.hitzone = [self.rect.x-width*2.5,self.rect.y-height*2.5,self.rect.y+height*2.5,self.rect.y+height*2.5]
+        self.countdonw = 60
+
+    def bulletHit(self):
+        self.hp -= 1
+
+        if self.hp <0:
+            self.kill()
+
+    def update(self,bullets,player):
+        self.countdonw -= 1
+        if self.countdonw == 0:
+            vitesseX,vitesseY=calculDeLaVitesseProjectile(self.rect.right,self.rect.centery,player.rect.x,player.rect.y)
+            bullets.add(Bullet(self.rect.right, self.rect.centery, vitesseX, vitesseY))
+            self.countdonw = 60
--- Jeu avec forme final/Projectile.py
+++ Jeu avec forme final/Projectile.py
@@ -5,7 +5,7 @@ class Bullet(pygame.sprite.Sprite):
     def __init__(self, x, y, sx, sy):
         super().__init__()
         self.image = pygame.Surface((10, 20))
-        self.image.fill(BLACK)
+        self.image.fill(YELLOW)
         self.rect = self.image.get_rect()
         self.rect.bottom = y
         self.rect.centerx = x
