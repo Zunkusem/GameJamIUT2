2f890885285c731b3d7f7b294a736dbf81647cbb
EmilioBe098 <39830161+EmilioBe098@users.noreply.github.com>
2019-02-07 22:22:23 +0100

    cache

--- Jeu avec forme final/Jeu.py
+++ Jeu avec forme final/Jeu.py
@@ -9,7 +9,6 @@ from Score import *
 from Fin import *
 import Fin
 
-
 def main():
     """ Main Program """
     pygame.init()
@@ -39,7 +38,10 @@ def main():
     level_list = []
     level_list.append(Tuto(player))
     level_list.append(Level_01(player))
->>>>>>> 1cf801ee5a90345ba7475304bffb4f5af544f0d5
+    
+    
+    
+    
 
     # Set the current level
     current_level_no = 0
@@ -78,8 +80,6 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
-                pygame.quit()
-
             elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 print("x:",event.pos[0]," y:",event.pos[1])
                 xSouris=event.pos[0]
@@ -101,8 +101,7 @@ def main():
                     player.jump()
                 if event.key == pygame.K_SPACE:
                     player.invG()
-                if event.key == pygame.K_ESCAPE:
-                    launcher()
+
 
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_a and player.change_x < 0:
@@ -179,8 +178,7 @@ def main():
 
     # Be IDLE friendly. If you forget this line, the program will 'hang'
     # on exit.
-    #score_final = player.level.score.getScore() #a decommenter lors du rassemblage avec forpec
-    score_final = 100
+    score_final = player.level.score.getScore() #a decommenter lors du rassemblage avec forpec
     fin(score_final)
     #return player.level.score.getScore() #a decommenter lors du rassemblage avec forpec
     pygame.quit()
