0a3f53e415d6510d642a867715edbdb746fcad80
Zunkusem <jeremi04@hotmail.fr>
2019-02-07 22:32:35 +0100

    Reparation

--- Jeu avec forme final/Jeu.py
+++ Jeu avec forme final/Jeu.py
@@ -9,6 +9,7 @@ from Score import *
 from Fin import *
 import Fin
 
+
 def main():
     """ Main Program """
     pygame.init()
@@ -38,10 +39,6 @@ def main():
     level_list = []
     level_list.append(Tuto(player))
     level_list.append(Level_01(player))
-    
-    
-    
-    
 
     # Set the current level
     current_level_no = 0
@@ -80,6 +77,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
+                pygame.quit()
+
             elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 print("x:",event.pos[0]," y:",event.pos[1])
                 xSouris=event.pos[0]
@@ -101,7 +100,8 @@ def main():
                     player.jump()
                 if event.key == pygame.K_SPACE:
                     player.invG()
-
+                if event.key == pygame.K_ESCAPE:
+                    launcher()
 
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_a and player.change_x < 0:
@@ -113,7 +113,7 @@ def main():
         active_sprite_list.update(screen)
 
         # Update items in the level
-        current_level.update(player)
+        current_level.update(player,screen)
         # current_level.shift_worldy(player.change_y,player)
 
         # If the player gets near the right side, shift the world left (-x)
@@ -179,6 +179,7 @@ def main():
     # Be IDLE friendly. If you forget this line, the program will 'hang'
     # on exit.
     score_final = player.level.score.getScore() #a decommenter lors du rassemblage avec forpec
+    #score_final = 100
     fin(score_final)
     #return player.level.score.getScore() #a decommenter lors du rassemblage avec forpec
     pygame.quit()
